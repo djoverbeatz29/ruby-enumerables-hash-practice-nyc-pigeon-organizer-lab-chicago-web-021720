ce0e82db91e0025ca3f480bbddf6a9ca56e337a7
Jack Overby <jroverby92@gmail.com>
2020-01-14 09:46:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+pigeon_data.map{ |k1, v1|
+  v1.map{ |k2, v2|
+    v2.map{ |item|
+      Hash[item, Hash[k1, k2]]
+    }
+  }
+}
+  
 end
