64aa30e1cf6833c44652d9e04e23de50529cb241
Jack Overby <jroverby92@gmail.com>
2020-01-14 10:08:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,15 +6,10 @@ result = {}
 data.each{ |k1, v1|
   v1.each{ |k2, v2|
     v2.each{ |item|
-      (result[item][k1]) ? 
+      if result[item][k1]
+        
     }
   }
 }
-
-for x in cf do
-  for y in x do
-    for z in y do
-      for k in z.keys do
-        if result[k]
   
 end
