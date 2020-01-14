2ec8122079bf89c0f69e184196190f9553975d9d
Jack Overby <jroverby92@gmail.com>
2020-01-14 10:12:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-result = {}
-  
-data.each{ |k1, v1|
-  v1.each{ |k2, v2|
-    v2.each{ |item|
-      if !result[item]
-        result[item] = {k1: [k2]}
-      else
-        result[item][k1].push(k2)
-      end
+  result = {}
+    
+  data.each{ |k1, v1|
+    v1.each{ |k2, v2|
+      v2.each{ |item|
+        if !result[item]
+          result[item] = {k1: [k2]}
+        else
+          result[item][k1].push(k2)
+        end
+      }
     }
   }
-}
   
 end
